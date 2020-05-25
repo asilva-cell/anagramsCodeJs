3aafee4a7138e84ddb3446990072204cc87f2a74
asilva-cell <achapellin@gmail.com>
2020-05-25 16:56:07 -0600

    removed unused expressions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function removeNonAplhaChar(item) {
-	return item.toLowerCase().replace(/[^a-z0-9+]+/gi, "");
+	return item.replace(/[^a-z0-9+]+/gi, "");
 }
 
 function sortAlphabetically(str) {
@@ -8,24 +8,25 @@ function sortAlphabetically(str) {
 		.join("");
 }
 
-function addToPotentialAnagrams(anagramList, sortedStr, item) {
-	sortedStr in anagramList
-		? anagramList[sortedStr].push(item)
-		: (anagramList[sortedStr] = [item]);
-	return anagramList;
+function addToPotentialAnagrams(anagramsList, sortedStr, item) {
+	if (!anagramsList[sortedStr]) {
+		anagramsList[sortedStr] = [];
+	}
+	anagramsList[sortedStr].push(item);
+	return anagramsList;
 }
 
-function checkForAnagrams(nestedInput) {
+const anagrams = nestedInput => {
 	let posibleAnagrams = {};
-	nestedInput.map(item => {
+	nestedInput.forEach(item => {
 		let sortedStr = sortAlphabetically(removeNonAplhaChar(item));
-		addToPotentialAnagrams(posibleAnagrams, sortedStr, item);
+		posibleAnagrams = addToPotentialAnagrams(
+			posibleAnagrams,
+			sortedStr,
+			item
+		);
 	});
 	return Object.values(posibleAnagrams).filter(list => list.length > 1);
-}
-
-const anagrams = input => {
-	return checkForAnagrams(input);
 };
 
 if (process.env.NODE_ENV !== "test") {
